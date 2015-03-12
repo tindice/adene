930b6ac8a7f247fe52de6a431a24eeb0e63bbe68
Martin Leibner <tincorad@gmail.com>
2015-03-11 22:12:52 -0300

    Moviendo funcionalidad fortune a su propio modulo

--- adene.js
+++ adene.js
@@ -5,14 +5,7 @@ var app = express();
 var handlebars = require('express3-handlebars')
     .create({ defaultLayout:'main' });
  
-//--------------------------------------------
-var fortunes = [
-"Conquer your fears or they will conquer you.",
-"Rivers need springs.",
-"Do not fear what you don't know.",
-"You will have a pleasant surprise.",
-"Whenever possible, keep it simple.",
-]; 
+var fortune = require('./lib/fortune.js');
     
 app.engine('handlebars', handlebars.engine);
 
@@ -30,8 +23,7 @@ app.get('/', function(req, res){
 });
 
 app.get('/about', function(req, res){
-    var randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)];
-    res.render('about', { fortune: randomFortune });
+    res.render('about', { fortune: fortune.getFortune() });
 });
 
 // custom 404 page
